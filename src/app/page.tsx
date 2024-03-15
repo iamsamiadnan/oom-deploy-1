3ab72999d082e900728245d1c53ae26a8f1ae33c
iamsamiadnan <sami.git@aol.com>
2024-03-15 10:35:39 +0600

    fix(i18n type): fix i18n typescript issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 'use client'
 
 import useTranslation from '@/lib/i18n/useTranslation'
-import { useEffect, useState } from 'react';
-
+import { Dispatch, SetStateAction, useState } from 'react';
 
 export default function Home() {
   const [foo, setFoo] = useState(false);
+  const {t, toggle, setToggle} = useTranslation();
 
-  const t: any = useTranslation();
+
 
 
   return (
     <>
-    <button onClick={() => setFoo(!foo)}>Click Me</button>
-    <h1>{t?.settings.project_information.project_name} {foo ? 'true' : 'false'}</h1>
+    <button onClick={() => setToggle(!toggle)}>Click Me</button>
+    <h1>{t.settings.project_information.project_name} {foo ? 'true' : 'false'}</h1>
     </>
   );
 
