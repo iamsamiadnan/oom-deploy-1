83c41001211ab1d652ff2a20d7749d8c42131eeb
iamsamiadnan <sami.git@aol.com>
2024-03-21 14:28:06 +0600

    deploy to vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
+
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import Provider from "./Provider";
-import AppBar from "./AppBar";
+import Image from "next/image";
 
 const poppins = Poppins({
   weight: ["400", "500", "600", "700"],
@@ -18,20 +19,35 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  params,
 }: Readonly<{
   children: React.ReactNode;
-  params: any;
 }>) {
-  console.log(params, "params");
-
   return (
     <html lang="en">
       <body className={poppins.className}>
         <div>
           <Provider>
             {/* <AppBar /> */}
-            <AntdRegistry>{children}</AntdRegistry>
+            <AntdRegistry>
+              <div className="max-w-[393px] mx-auto relative">
+                <header className="flex justify-between h-[57px] items-center bg-white px-4 absolute top-0 w-full z-10">
+                  <Image
+                    src="/Icons/FunnelSimple.svg"
+                    width="24"
+                    height="24"
+                    alt=""
+                  />
+                  <Image src="/oom-logo.png" width="132" height="32" alt="" />
+                  <Image
+                    src="/Icons/ShoppingBag.svg"
+                    width="24"
+                    height="24"
+                    alt=""
+                  />
+                </header>
+                <main className="bg-white h-screen">{children}</main>
+              </div>
+            </AntdRegistry>
           </Provider>
         </div>
       </body>
